a37473c30dd0ab634277313df6ff11e936023e91
Alexandre Thomassin <alex45thomassin@gmail.com>
2024-01-14 15:28:15 +0100

    Init commit

---
+++ DS_Medev/src/main/java/edu/centrale/ds_medev/DS_Medev.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.centrale.ds_medev;
+
+/**
+ *
+ * @author alex4
+ */
+public class DS_Medev {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
