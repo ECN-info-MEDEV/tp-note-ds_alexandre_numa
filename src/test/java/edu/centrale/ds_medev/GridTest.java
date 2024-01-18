8d2aee97ae40d705edf59c59abce1b4529780fe3
numa-pergeaux <pergeauxnuma@gmail.com>
2024-01-18 10:46:25 +0100

    Update GridTest.java

--- src/test/java/edu/centrale/ds_medev/GridTest.java
+++ src/test/java/edu/centrale/ds_medev/GridTest.java
@@ -49,5 +49,117 @@ public class GridTest {
         
         assertEquals(5, grid.getSize());
     }
+
+    /**
+     * Test of getCells method, of class Grid.
+     */
+    @Test
+    public void testGetCells() {
+        System.out.println("getCells");
+        Grid instance = null;
+        Cell[][] expResult = null;
+        Cell[][] result = instance.getCells();
+        assertArrayEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setCells method, of class Grid.
+     */
+    @Test
+    public void testSetCells() {
+        System.out.println("setCells");
+        Cell[][] cells = null;
+        Grid instance = null;
+        instance.setCells(cells);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of displayGrid method, of class Grid.
+     */
+    @Test
+    public void testDisplayGrid() {
+        System.out.println("displayGrid");
+        Grid instance = null;
+        instance.displayGrid();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of markHit method, of class Grid.
+     */
+    @Test
+    public void testMarkHit() {
+        System.out.println("markHit");
+        int x = 0;
+        int y = 0;
+        Grid instance = null;
+        instance.markHit(x, y);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of markMiss method, of class Grid.
+     */
+    @Test
+    public void testMarkMiss() {
+        System.out.println("markMiss");
+        int x = 0;
+        int y = 0;
+        Grid instance = null;
+        instance.markMiss(x, y);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of isOccupied method, of class Grid.
+     */
+    @Test
+    public void testIsOccupied() {
+        System.out.println("isOccupied");
+        int x = 0;
+        int y = 0;
+        Grid instance = null;
+        boolean expResult = false;
+        boolean result = instance.isOccupied(x, y);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setOccupied method, of class Grid.
+     */
+    @Test
+    public void testSetOccupied() {
+        System.out.println("setOccupied");
+        int x = 0;
+        int y = 0;
+        Grid instance = null;
+        instance.setOccupied(x, y);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of setShip method, of class Grid.
+     */
+    @Test
+    public void testSetShip() {
+        System.out.println("setShip");
+        int x = 0;
+        int y = 0;
+        Ship ship = null;
+        Grid instance = null;
+        instance.setShip(x, y, ship);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
 }
 
