f381995c22b750bf2673636db0e5c740739f53b3
Alexandre Thomassin <alex45thomassin@gmail.com>
2024-01-18 11:20:26 +0100

    Update test

--- src/test/java/edu/centrale/ds_medev/GridTest.java
+++ src/test/java/edu/centrale/ds_medev/GridTest.java
@@ -54,117 +54,117 @@ public class GridTest {
     /**
      * Test of getCells method, of class Grid.
      */
-    @Disabled
-    @Test
-    public void testGetCells() {
-        System.out.println("getCells");
-        Grid instance = null;
-        Cell[][] expResult = null;
-        Cell[][] result = instance.getCells();
-        assertArrayEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Disabled
+//    @Test
+//    public void testGetCells() {
+//        System.out.println("getCells");
+//        Grid instance = null;
+//        Cell[][] expResult = null;
+//        Cell[][] result = instance.getCells();
+//        assertArrayEquals(expResult, result);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of setCells method, of class Grid.
      */
-    @Disabled
-    @Test
-    public void testSetCells() {
-        System.out.println("setCells");
-        Cell[][] cells = null;
-        Grid instance = null;
-        instance.setCells(cells);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Disabled
+//    @Test
+//    public void testSetCells() {
+//        System.out.println("setCells");
+//        Cell[][] cells = null;
+//        Grid instance = null;
+//        instance.setCells(cells);
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
     /**
      * Test of displayGrid method, of class Grid.
      */
-    @Disabled
-    @Test
-    public void testDisplayGrid() {
-        System.out.println("displayGrid");
-        Grid instance = null;
-        instance.displayGrid();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+//    @Disabled
+//    @Test
+//    public void testDisplayGrid() {
+//        System.out.println("displayGrid");
+//        Grid instance = null;
+//        instance.displayGrid();
+//        // TODO review the generated test code and remove the default call to fail.
+//        fail("The test case is a prototype.");
+//    }
 
 
     /**
      * Test of markHit method, of class Grid.
      */
-    @Disabled
-    @Test
-    public void testMarkHit() {
-        System.out.println("markHit");
-        Grid grid = new Grid(5);
-
-        grid.markHit(1, 4);
-        Cell[][] cells = grid.getCells();
-        assertTrue((cells[1][4]).isHit());
-    }
+//    @Disabled
+//    @Test
+//    public void testMarkHit() {
+//        System.out.println("markHit");
+//        Grid grid = new Grid(5);
+//
+//        grid.markHit(1, 4);
+//        Cell[][] cells = grid.getCells();
+//        assertTrue((cells[1][4]).isHit());
+//    }
 
     /**
      * Test of markMiss method, of class Grid.
      */
-    @Disabled
-    @Test
-    public void testMarkMiss() {
-        System.out.println("markMiss");
-        Grid grid = new Grid(5);
-
-        grid.markMiss(1, 4);
-        Cell[][] cells = grid.getCells();
-        assertTrue((cells[1][4]).isMiss());
-    }
+//    @Disabled
+//    @Test
+//    public void testMarkMiss() {
+//        System.out.println("markMiss");
+//        Grid grid = new Grid(5);
+//
+//        grid.markMiss(1, 4);
+//        Cell[][] cells = grid.getCells();
+//        assertTrue((cells[1][4]).isMiss());
+//    }
 
     /**
      * Test of isOccupied method, of class Grid.
      */
-    @Disabled
-    @Test
-    public void testIsOccupied() {
-        System.out.println("isOccupied");
-        Grid grid = new Grid(5);
-        
-        grid.getCells()[1][4].setOccupied(true);
-        grid.getCells()[4][1].setOccupied(false);
-        assertTrue(grid.getCells()[1][4].isOccupied());
-        assertFalse(grid.getCells()[4][1].isOccupied());
-    }
+//    @Disabled
+//    @Test
+//    public void testIsOccupied() {
+//        System.out.println("isOccupied");
+//        Grid grid = new Grid(5);
+//        
+//        grid.getCells()[1][4].setOccupied(true);
+//        grid.getCells()[4][1].setOccupied(false);
+//        assertTrue(grid.getCells()[1][4].isOccupied());
+//        assertFalse(grid.getCells()[4][1].isOccupied());
+//    }
 
     /**
      * Test of setOccupied method, of class Grid.
      */
-    @Disabled
-    @Test
-    public void testSetOccupied() {
-        System.out.println("setOccupied");
-        Grid grid = new Grid(5);
-        
-        grid.getCells()[1][4].setOccupied(true);
-        grid.getCells()[4][1].setOccupied(false);
-        assertTrue(grid.getCells()[1][4].isOccupied());
-        assertFalse(grid.getCells()[4][1].isOccupied());
-    }
+//    @Disabled
+//    @Test
+//    public void testSetOccupied() {
+//        System.out.println("setOccupied");
+//        Grid grid = new Grid(5);
+//        
+//        grid.getCells()[1][4].setOccupied(true);
+//        grid.getCells()[4][1].setOccupied(false);
+//        assertTrue(grid.getCells()[1][4].isOccupied());
+//        assertFalse(grid.getCells()[4][1].isOccupied());
+//    }
 
     /**
      * Test of setShip method, of class Grid.
      */
-    @Disabled
-    @Test
-    public void testSetShip() {
-        System.out.println("setShip");
-        Grid grid = new Grid(5);
-        
-        grid.setShip(1, 4, new Ship("Ship", 4, new Cell[4])); 
-        
-        assertEquals("Ship",grid.getCells()[1][4].getShip().getType());
-        assertEquals(4,grid.getCells()[1][4].getShip().getLength());
-    }
+//    @Disabled
+//    @Test
+//    public void testSetShip() {
+//        System.out.println("setShip");
+//        Grid grid = new Grid(5);
+//        
+//        grid.setShip(1, 4, new Ship("Ship", 4, new Cell[4])); 
+//        
+//        assertEquals("Ship",grid.getCells()[1][4].getShip().getType());
+//        assertEquals(4,grid.getCells()[1][4].getShip().getLength());
+//    }
 }
 
